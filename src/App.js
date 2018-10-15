7bf4ae5dc5845309763c131dc489194115e85da3
amitms <amitms2005@gmail.com>
2018-10-15 18:03:29 -0400

    basic map react components restructured

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    mapIsReady: false
+    };
+  }
 
   componentDidMount() {
-    this.renderMap()
-  }
-
-  renderMap = () => {
     const ApiKey = 'AIzaSyBrRQlBPiy6icvdiqbmIrj0DQ1RuI1FKEM';  //AIzaSyAD4vpwyw4zFgzo_4_RG4lAaVwCIVZM9Jc
-    loadScript(`https://maps.googleapis.com/maps/api/js?key=${ApiKey}&v=3&callback=initMap`);
-    window.initMap = this.initMap
+    var index = window.document.getElementsByTagName("script")[0];
+    var script = window.document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}&v=3`;
+    script.async = true;
+    script.defer = true;
+    script.addEventListener('load', () => {
+      this.setState({ mapIsReady: true });
+    });
+    index.parentNode.insertBefore(script, index);
   }
 
-  initMap = () =>{
+  componentDidUpdate() {
+    const home = {lat: 39.2356323, lng: -85.8651852};
+
+    if (this.state.mapIsReady) {
+      var map = new window.google.maps.Map(document.getElementById('map'), {
+        center: home,          
+        zoom: 14,
+        mapTypeId: 'roadmap'
+      });
     // eslint-disable-next-line
-        const home = {lat: 39.2356323, lng: -85.8651852};
-        var map = new window.google.maps.Map(document.getElementById('map'), {
-          center: home,          
-          zoom: 14
-        });
-        const marker = new window.google.maps.Marker({
+      const marker = new window.google.maps.Marker({
         position: home,
         map: map
-    });
-      }
+      });
+    }
+  }
 
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+            Neighbourhood Map - React -Udacity
+        </header>
+
         <div id="map"></div>
-      </div>     
+      </div>    
 
       )
   }
 }
-function loadScript(url){
-  var index = window.document.getElementsByTagName("script")[0]
-  var script = window.document.createElement("script")
-  script.src = url
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
 
-}
 
 export default App;
